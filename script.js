de4d45ebd2c1492b3769a319ccd687e27b35c452
Idriz <idrizosmanii6@gmail.com>
2024-03-09 17:01:04 +0100

    changed videos

--- script.js
+++ script.js
@@ -31,61 +31,49 @@ forma.reset()
 // Define function to be called when button is clicked
 // Define function to update the video link based on inputs
 function updateVideoLink() {
-  const weightInput = parseFloat(document.getElementById("weightKg").value);
-  const heightInput = parseFloat(document.getElementById("heightCm").value);
   const getBtn = document.getElementById("getBtn");
   const muscularRadio = document.getElementById("muscular");
   const cardioRadio = document.getElementById("cardio");
   const thinnerRadio = document.getElementById("thinner");
 
-  if (weightInput && heightInput) {
-    //fat short
-    if (weightInput >= 90 && heightInput <= 185) {
-      if (muscularRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=TuRv8gRfvb8';
-      } else if (cardioRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
-      } else {
-        getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
-      }
-    }
+  const weightInput = parseFloat(document.getElementById("weightKg").value);
+  const heightInput = parseFloat(document.getElementById("heightCm").value);
 
-    //fat tall
-    if (weightInput >= 100 && heightInput >= 185) {
-      if (muscularRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=TuRv8gRfvb8';
-      } else if (cardioRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
-      } else {
-        getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
-      }
+  if (weightInput >= 90 && heightInput <= 185) {
+    if (muscularRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=TuRv8gRfvb8';
+    } else if (cardioRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
+    } else {
+      getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
     }
-
-    //skinny tall
-    if (weightInput <= 60 && heightInput >= 185) {
-      if (muscularRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=an0rksMcJoA';
-      } else if (cardioRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
-      } else {
-        getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
-      }
+  } else if (weightInput >= 100 && heightInput >= 185) {
+    if (muscularRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=TuRv8gRfvb8';
+    } else if (cardioRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
+    } else {
+      getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
     }
-
-    //skinny short
-    if (weightInput >= 90 && heightInput <= 185) {
-      if (muscularRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=an0rksMcJoA';
-      } else if (cardioRadio.checked) {
-        getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
-      } else {
-        getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
-      }
+  } else if (weightInput <= 60 && heightInput >= 185) {
+    if (muscularRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=PYVxaMyfSTc&pp=ygUMYnVsayB1cCBmYXN0';
+    } else if (cardioRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
+    } else {
+      getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
+    }
+  } else {
+    if (muscularRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=PYVxaMyfSTc&pp=ygUMYnVsayB1cCBmYXN0';
+    } else if (cardioRadio.checked) {
+      getBtn.href = 'https://www.youtube.com/watch?v=ZMO_XC9w7Lw';
+    } else {
+      getBtn.href = 'https://www.youtube.com/watch?v=NQmW-VQHToc';
     }
   }
 }
 
-// Add event listeners to input fields and radio buttons
 const weightInput = document.getElementById("weightKg");
 const heightInput = document.getElementById("heightCm");
 const radioButtons = document.querySelectorAll('input[type="radio"]');
